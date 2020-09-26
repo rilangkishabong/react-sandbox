1d37279a65f1d6927838b8278f12a2ced32064c1
Rilangki <rilangkishabong@gmail.com>
2020-09-26 15:24:10 +0530

    Board handles the state of the game.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null
-        };
-    }
 
     render() {
         return (
-            <button className="square" onClick={() => this.setState({value: "X"})}>
-                {this.state.value}
+            <button className="square"
+                    onClick={() => this.props.onClick()}
+            >
+                {this.props.value}
             </button>
         );
     }
@@ -26,6 +22,13 @@ class Board extends React.Component {
             squares: Array(9).fill(null)
         };
     }
+
+     handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = "X";
+        this.setState({squares: squares});
+    }
+
 
     renderSquare(i) {
         return <Square value={this.state.squares[i]}
