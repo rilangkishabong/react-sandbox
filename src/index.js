2ede56310fe4b950bbebff377136cee4ac88d450
Rilangki <rilangkishabong@gmail.com>
2020-09-28 18:05:34 +0530

    toggle a button

--- src/index.js
+++ src/index.js
@@ -2,46 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Clock extends React.Component{
+class Toggle extends React.Component {
     constructor(props) {
-        console.log("constructor")
         super(props);
-        this.state = {date: new Date()};
+        this.state = {isToggleOn: true};
+
+        // This binding is necessary to make `this` work in the callback
+        this.handleClick = this.handleClick.bind(this);
     }
-    componentDidMount() {
-        console.log("mount")
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
+
+    handleClick() {
+        this.setState(state => ({
+            isToggleOn: !state.isToggleOn
+        }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
         );
-    }
-
-    componentWillUnmount() {
-        console.log("unmount")
-        clearInterval(this.timerID);
-
-    }
-
-    tick() {
-        console.log("tick")
-        this.setState({
-            date: new Date()
-        });
-        // this.state.date = new Date();
-        // console.log(this.state.date.toLocaleDateString());
-    }
-
-    render()
-    {
-        console.log("render")
-        return(
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        )
     }
 }
 
-ReactDOM.render(<Clock />
-        , document.getElementById('root'));
+ReactDOM.render(
+    <Toggle />,
+    document.getElementById('root')
+);
