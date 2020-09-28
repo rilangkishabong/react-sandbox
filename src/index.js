d2f1f3c3ec553b6265e1bb960bc4f481354b6262
Rilangki <rilangkishabong@gmail.com>
2020-09-28 14:50:27 +0530

    timer with component with state

--- src/index.js
+++ src/index.js
@@ -2,7 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(
-    <h1>Hello world</h1>,
-    document.getElementById("root")
-)
+class Clock extends React.Component{
+    constructor(props) {
+        console.log("constructor")
+        super(props);
+        this.state = {date: new Date()};
+    }
+    componentDidMount() {
+        console.log("mount")
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        console.log("unmount")
+        clearInterval(this.timerID);
+
+    }
+
+    tick() {
+        console.log("tick")
+        this.setState({
+            date: new Date()
+        });
+        // this.state.date = new Date();
+        // console.log(this.state.date.toLocaleDateString());
+    }
+
+    render()
+    {
+        console.log("render")
+        return(
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Clock />
+        , document.getElementById('root'));
