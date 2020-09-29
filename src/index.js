1f04d0cf7a8c9457780ff8b0c2abb7ae9bf684ac
Rilangki <rilangkishabong@gmail.com>
2020-09-29 12:30:51 +0530

    "select" as a controlled component.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class EssayForm extends React.Component {
+class FlavorForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            value: 'Please write an essay about your favorite DOM element.'
-        };
+        this.state = {value: 'coconut'};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -18,7 +16,7 @@ class EssayForm extends React.Component {
     }
 
     handleSubmit(event) {
-        alert('An essay was submitted: ' + this.state.value);
+        alert('Your favorite flavor is: ' + this.state.value);
         event.preventDefault();
     }
 
@@ -26,8 +24,13 @@ class EssayForm extends React.Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                    Essay:
-                    <textarea value={this.state.value} onChange={this.handleChange} />
+                    Pick your favorite flavor:
+                    <select value={this.state.value} onChange={this.handleChange}>
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
                 </label>
                 <input type="submit" value="Submit" />
             </form>
@@ -35,6 +38,6 @@ class EssayForm extends React.Component {
     }
 }
 ReactDOM.render(
-    <EssayForm />,
+    <FlavorForm />,
     document.getElementById('root')
 );
