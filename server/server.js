152a03b28b876c7a67093340dd810988ed404f6b
Rilangki <rilangkishabong@gmail.com>
2020-09-30 18:29:01 +0530

    set up mongoose with basic schema & queries.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,39 @@
-var http = require("http");
+//require mongoose & connect to mongodb
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/testdb', {useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
 
-http.createServer(function (request, response) {
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+//connection and callbacks
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("mongodb connected");
+    // we're connected!
+});
 
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
-}).listen(8081);
+//schema for kitty
+const kittySchema = new mongoose.Schema({
+    name: String
+});
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+//bind functions to schema before using
+kittySchema.methods.speak = function () {
+    const greeting = this.name
+        ? "Meow name is " + this.name
+        : "I don't have a name";
+    console.log(greeting);
+}
+
+//create model(a class based on the kittySchema)
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+//create an object based on the model.
+const silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+
+const fluffy = new Kitten({ name: 'fluffy' });
+fluffy.speak(); // "Meow name is fluffy"
+
+fluffy.save(function (err, fluffy) {
+    if (err) return console.error(err);
+    fluffy.speak();
+});
