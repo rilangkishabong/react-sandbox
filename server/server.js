e26db2c2f6962fefe54c03310d163f20191401d1
Rilangki <rilangkishabong@gmail.com>
2020-09-30 15:38:02 +0530

    read userList and post addUser

--- server/server.js
+++ server/server.js
@@ -1,28 +1,38 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var fs = require("fs");
 
-// Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-app.use(express.static('public'));
-app.get('/index.htm', function (req, res) {
-    res.sendFile( __dirname + "/" + "index.htm" );
+var user = {
+    "user4" : {
+        "name" : "mohit",
+        "password" : "password4",
+        "profession" : "teacher",
+        "id": 4
+    }
+}
+
+app.post('/addUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/model" + "/users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        data["user4"] = user["user4"];
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
 })
 
-app.post('/process_post', urlencodedParser, function (req, res) {
-    // Prepare output in JSON format
-    response = {
-        first_name:req.body.first_name,
-        last_name:req.body.last_name
-    };
-    console.log(response);
-    res.end(JSON.stringify(response));
+app.get('/listUsers', function (req, res) {
+    fs.readFile( __dirname + "/model/" + "users.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    });
 })
+
+
 
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
-
     console.log("Example app listening at http://%s:%s", host, port)
 })
