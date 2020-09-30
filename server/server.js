2fa9309185e2f62304ce65c026f97b3f8ebd110c
Rilangki <rilangkishabong@gmail.com>
2020-09-30 15:59:04 +0530

    search a particular user.

--- server/server.js
+++ server/server.js
@@ -2,34 +2,15 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-
-var user = {
-    "user4" : {
-        "name" : "mohit",
-        "password" : "password4",
-        "profession" : "teacher",
-        "id": 4
-    }
-}
-
-app.post('/addUser', function (req, res) {
+app.get('/:id', function (req, res) {
     // First read existing users.
-    fs.readFile( __dirname + "/model" + "/users.json", 'utf8', function (err, data) {
-        data = JSON.parse( data );
-        data["user4"] = user["user4"];
-        console.log( data );
-        res.end( JSON.stringify(data));
-    });
-})
-
-app.get('/listUsers', function (req, res) {
     fs.readFile( __dirname + "/model/" + "users.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
+        var users = JSON.parse(data);
+        var user = users["user" + req.params.id]
+        console.log(user);
+        res.end( JSON.stringify(user));
     });
-})
-
-
+});
 
 var server = app.listen(8081, function () {
     var host = server.address().address
