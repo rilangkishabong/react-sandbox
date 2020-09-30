bd73aba6d910c451f62db48a609ce9d99735c01f
Rilangki <rilangkishabong@gmail.com>
2020-09-30 16:02:09 +0530

    delete a particular user.

--- server/server.js
+++ server/server.js
@@ -2,15 +2,18 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-app.get('/:id', function (req, res) {
+var id = 2;
+
+app.delete('/deleteUser', function (req, res) {
     // First read existing users.
     fs.readFile( __dirname + "/model/" + "users.json", 'utf8', function (err, data) {
-        var users = JSON.parse(data);
-        var user = users["user" + req.params.id]
-        console.log(user);
-        res.end( JSON.stringify(user));
+        data = JSON.parse( data );
+        delete data["user" + 2];
+
+        console.log( data );
+        res.end( JSON.stringify(data));
     });
-});
+})
 
 var server = app.listen(8081, function () {
     var host = server.address().address
