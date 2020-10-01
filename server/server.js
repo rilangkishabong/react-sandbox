346a0301d3ac92968265f8b36ce7ac2628af3c6f
Rilangki <rilangkishabong@gmail.com>
2020-10-01 13:03:40 +0530

    complete simple user registration & login using mongoose.

--- server/server.js
+++ server/server.js
@@ -1,39 +1,92 @@
-//require mongoose & connect to mongodb
+const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/testdb', {useNewUrlParser: true, useUnifiedTopology: true });
+
+//connect to mongodb
+mongoose.connect('mongodb://localhost/testdb', {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
 
 //connection and callbacks
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', function () {
     console.log("mongodb connected");
     // we're connected!
 });
 
 //schema for kitty
-const kittySchema = new mongoose.Schema({
-    name: String
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
 });
-
-//bind functions to schema before using
-kittySchema.methods.speak = function () {
-    const greeting = this.name
-        ? "Meow name is " + this.name
-        : "I don't have a name";
-    console.log(greeting);
-}
 
 //create model(a class based on the kittySchema)
-const Kitten = mongoose.model('Kitten', kittySchema);
+const User = mongoose.model('User', userSchema);
 
-//create an object based on the model.
-const silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
+app.get('/register', function (req, res) {
+    res.sendFile(__dirname + "/index.html", function (err) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("Lets register")
+            }
+        }
+    )
+})
+app.post('/register', function (req, res) {
+    const userName = req.body.userName;
+    const passWord = req.body.password;
 
-const fluffy = new Kitten({ name: 'fluffy' });
-fluffy.speak(); // "Meow name is fluffy"
+    const ek_user = new User({username: userName, password: passWord});
 
-fluffy.save(function (err, fluffy) {
-    if (err) return console.error(err);
-    fluffy.speak();
-});
+    ek_user.save(function (err, ek_user) {
+        if (err) return console.error(err);
+        console.log("User is registered!");
+        res.send("Registered!");
+    });
+})
+
+app.get('/login', function (req, res) {
+    res.sendFile(__dirname + "/login.html", function (err) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("Lets login")
+            }
+        }
+    )
+})
+
+app.post('/login', function (req, res) {
+    const userName = req.body.userName;
+    const passWord = req.body.password;
+
+    User.findOne({username: userName}, function (err, user) {
+        if (err) {
+            return console.error(err);
+            res.send("database error");
+
+        }
+        console.log(user);
+        if (user) {
+            if (user.password === passWord) {
+                res.status(200).send("welcome back");
+            } else {
+                res.status(401).send("Invalid password");
+            }
+        } else{
+            res.send("User does not exist");
+        }
+    })
+
+    // res.send(User.findOne({username: userName}));
+
+})
+
+app.listen(3000, function (err) {
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port 3000");
+})
